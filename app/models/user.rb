1c6ec67e89851811f71f0c918ef804abee88f866
sharkofecuis <sharkofecuis@gmail.com>
2016-12-27 08:13:01 -0800

    join & quit group action

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,15 +5,15 @@ class User < ApplicationRecord
 
 
 
-  #devise :database_authenticatable, :registerable,
-         #:recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 
-         #has_many :group_relationships
-        #has_many :participated_groups, :through => :group_relationships, :source => :group
+         has_many :group_relationships
+        has_many :participated_groups, :through => :group_relationships, :source => :group
 
-        #def is_member_of?(group)
-            # participated_groups.include?(group)
-           #end
+        def is_member_of?(group)
+            participated_groups.include?(group)
+           end
 
 
            def join!(group)
